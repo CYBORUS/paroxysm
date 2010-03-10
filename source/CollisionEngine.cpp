17b5950929969b17ad12a76eb44e25a8d689d0ea
Cdaragorn1 <Cdaragorn1@1a4693bc-ef48-11de-b206-85b67c77eac7>
2010-03-10 01:14:44 +0000

    Collisions seem to be fairly realistic now.
    git-svn-id: https://paroxysm.googlecode.com/svn/trunk@338 1a4693bc-ef48-11de-b206-85b67c77eac7

--- source/CollisionEngine.cpp
+++ source/CollisionEngine.cpp
@@ -19,6 +19,16 @@ void CollisionEngine::checkCollisions()
             {
                 mEntities[i]->onCollision(mEntities[j]);
             }
+
+            //after we've had one of the two objects deal with it's collision, test to see if there's still a collision
+            distance = (mEntities[i]->getPosition() - mEntities[j]->getPosition()).length();
+
+            if (distance < mEntities[i]->getRadius() + mEntities[j]->getRadius())
+            {
+                //if there is still a collision, call the second objects collision method
+                mEntities[j]->onCollision(mEntities[i]);
+            }
+
         }
     }
 }
