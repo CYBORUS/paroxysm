bba970565ef7d00b887c4b4e8ca0aefa13033cbd
TheBuzzSaw <TheBuzzSaw@1a4693bc-ef48-11de-b206-85b67c77eac7>
2010-03-13 05:46:47 +0000

    fixed GLEE redundancy
    git-svn-id: https://paroxysm.googlecode.com/svn/trunk@364 1a4693bc-ef48-11de-b206-85b67c77eac7

--- source/OGL.h
+++ source/OGL.h
@@ -18,25 +18,8 @@
 #ifndef OGL_H
 #define OGL_H
 
-#ifdef __WIN32__
-#define WIN32_LEAN_AND_MEAN
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
-#include <windows.h>
-#endif
-
-#if defined(__MACOSX__)
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
-#elif defined(__MACOS__)
-#include <gl.h>
-#include <glu.h>
-#else
+// GLEE does all the work for us. The only thing it leaves out is GLU.
 #include <GL/glee.h>
-#include <GL/gl.h>
-#include <GL/glext.h>
 #include <GL/glu.h>
-#endif
 
 #endif
