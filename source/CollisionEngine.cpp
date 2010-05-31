02829177c2f2f194d948470bf94f0c5e14fdf8eb
TheBuzzSaw <TheBuzzSaw@1a4693bc-ef48-11de-b206-85b67c77eac7>
2010-05-31 20:27:43 +0000

    small cleanup
    git-svn-id: https://paroxysm.googlecode.com/svn/trunk@427 1a4693bc-ef48-11de-b206-85b67c77eac7

--- source/CollisionEngine.cpp
+++ source/CollisionEngine.cpp
@@ -1,3 +1,20 @@
+/**
+ *  This file is part of "Paroxysm".
+ *
+ *  "Paroxysm" is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  "Paroxysm" is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with "Paroxysm".  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "CollisionEngine.h"
 
 list<Entity*> CollisionEngine::mEntities;
@@ -10,7 +27,6 @@ void CollisionEngine::onSetup()
 {
     mEntityLock = SDL_CreateMutex();
 }
-
 
 void CollisionEngine::addEntity(Entity* inEntity)
 {
@@ -35,7 +51,6 @@ void CollisionEngine::removeEntity(Entity* inEntity)
         ++it;
     }
 }
-
 
 void CollisionEngine::checkCollisions()
 {
