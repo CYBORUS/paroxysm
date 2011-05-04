8211956d9b743553b1655e4743e6dcd1275b270f
cdaragorn <cdaragorn1@gmail.com>
2011-05-03 21:09:30 -0400

    added entities and some other important files to be converted

---
+++ newsource/game/CollisionEngine.cpp
@@ -0,0 +1,162 @@
+/**
+ *  This file is part of "Paroxysm".
+ *
+ *  "Paroxysm" is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  "Paroxysm" is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with "Paroxysm".  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "CollisionEngine.h"
+
+list<Entity*> CollisionEngine::mEntities;
+float CollisionEngine::mLargestRadius = 0.0f;
+Functor CollisionEngine::mFunc;
+volatile bool CollisionEngine::mCollisionsRunning = false;
+SDL_mutex* CollisionEngine::mEntityLock;
+long CollisionEngine::mTimes = 0;
+
+void CollisionEngine::onSetup()
+{
+    //mEntities = inEntities;
+    mEntityLock = SDL_CreateMutex();
+    //this is a temporary fix, we need to find a way to choose
+    //a radius based on the list of entities
+    mLargestRadius = 0;
+}
+
+
+void CollisionEngine::addEntity(Entity* inEntity)
+{
+    SDL_mutexP(mEntityLock);
+    mEntities.push_back(inEntity->getReference());
+    SDL_mutexV(mEntityLock);
+    if (inEntity->getRadius() > mLargestRadius)
+    {
+        mLargestRadius = inEntity->getRadius() * 3.0f;
+    }
+    //inEntity->addReference();
+}
+
+int CollisionEngine::checkCollisions(void* unused)
+{
+    mCollisionsRunning = true;
+    //SDL_mutex* lock = (SDL_mutex*)inEntityLock;
+    //Vector3D<float> pos(10, 10, 0);
+    while (mCollisionsRunning)
+    {
+        //SDL_mutexP(lock);
+        //++mTimes;
+        SDL_mutexP(mEntityLock);
+        //list<Entity*> tempEntities = *mEntities;
+        //tempEntities.sort(mFunc);
+        mEntities.sort(mFunc);
+
+        list<Entity*>::iterator itFirst = mEntities.begin();
+        list<Entity*>::iterator itSecond;
+        list<Entity*>::iterator itEnd = mEntities.end();
+
+
+        while (itFirst != itEnd)
+        {
+            itSecond = itFirst;
+            ++itSecond;
+
+
+            Entity* first = *itFirst;
+            while (itSecond != mEntities.end())
+            {
+                Entity* second = *itSecond;
+                if (abs(first->getPosition()[0] - second->getPosition()[0]) > mLargestRadius)
+                {
+                    break;
+                }
+
+                float distance = (first->getPosition() - second->getPosition()).length();
+
+                if (distance < first->getRadius() + second->getRadius())
+                {
+                    first->onCollision(second);
+                    second->onCollision(first);
+                }
+
+/*
+                //after we've had one of the two objects deal with it's collision, test to see if there's still a collision
+                distance = (first->getPosition() - second->getPosition()).length();
+
+                if (distance < first->getRadius() + second->getRadius())
+                {
+                    //if there is still a collision, call the second objects collision method
+                    second->onCollision(first);
+                }
+                */
+
+                if (!second->isAlive())
+                {
+                    itSecond = mEntities.erase(itSecond);
+                    //second->setGameDead();
+                    second->removeReference();
+                }
+                else
+                {
+                    ++itSecond;
+                }
+            }
+
+            if (!first->isAlive())
+            {
+                itFirst = mEntities.erase(itFirst);
+                //first->setGameDead();
+                first->removeReference();
+            }
+            else
+            {
+                ++itFirst;
+            }
+        }
+        //SDL_mutexV(lock);
+        SDL_mutexV(mEntityLock);
+        SDL_Delay(1);
+        /*
+        for (unsigned int i = 0; i < mEntities.size() - 1; ++i)
+        {
+            for (unsigned int j = i + 1; j < mEntities.size(); ++j)
+            {
+                float distance = (mEntities[i]->getPosition() - mEntities[j]->getPosition()).length();
+
+                if (distance < mEntities[i]->getRadius() + mEntities[j]->getRadius())
+                {
+                    mEntities[i]->onCollision(mEntities[j]);
+                }
+
+                //after we've had one of the two objects deal with it's collision, test to see if there's still a collision
+                distance = (mEntities[i]->getPosition() - mEntities[j]->getPosition()).length();
+
+                if (distance < mEntities[i]->getRadius() + mEntities[j]->getRadius())
+                {
+                    //if there is still a collision, call the second objects collision method
+                    mEntities[j]->onCollision(mEntities[i]);
+                }
+            }
+        }
+        */
+    }
+    return 0;
+
+}
+
+
+void CollisionEngine::onUnload()
+{
+    //mCollisionsRunning = false;
+    mEntities.clear();
+    SDL_DestroyMutex(mEntityLock);
+}
---
+++ newsource/game/Entity.h
@@ -0,0 +1,172 @@
+/**
+ *  This file is part of "Paroxysm".
+ *
+ *  "Paroxysm" is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  "Paroxysm" is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with "Paroxysm".  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include "Vector3D.h"
+#include "TerrainGrid.h"
+
+
+class Entity
+{
+    public:
+        Entity();
+        virtual ~Entity();
+
+        enum EntityType { TANK, BULLET };
+
+        virtual void onCollision(Entity* inCollidedWith) = 0;
+        virtual void update();
+        virtual void move() = 0;
+        virtual void display() = 0;
+        virtual void changeDirection(float inDirection) = 0;
+        virtual void changeSpeed(float inSpeed) = 0;
+
+        static void setTerrain(TerrainGrid* inTerrain);
+
+        void setRadius(float inRadius);
+        float getRadius();
+
+        //void setGameDead();
+        //void setRenderDead();
+
+        void addReference();
+        Entity* getReference();
+        void removeReference();
+        bool hasReferences();
+
+        bool isGameDead();
+        bool isRenderDead();
+
+        const Vector3D<float>& getMomentum() const;
+        const Vector3D<float>& getPosition() const;
+        void setPosition(const Vector3D<float>& inPosition);
+
+        EntityType getWhatIAm();
+        bool isAlive();
+        void die();
+
+    protected:
+        EntityType mWhatAmI;
+        float mRadius;
+
+        char mNumReferences;
+
+        //Control* mControl;
+
+        //bool volatile mGameDead;
+        //bool volatile mRenderDead;
+
+        static TerrainGrid* mTerrain;
+        int mTerrainWidth;
+        int mTerrainHeight;
+
+
+        Vector3D<float> mMomentum;
+        Vector3D<float> mPosition;
+        Vector3D<float> mRotation;
+
+        //for remembering where we were in case of collisions
+        Vector3D<float> mPreviousPosition;
+
+        bool mAlive;
+
+};
+
+inline void Entity::die()
+{
+    mAlive = false;
+}
+inline const Vector3D<float>& Entity::getPosition() const
+{
+    return mPosition;
+}
+
+inline const Vector3D<float>& Entity::getMomentum() const
+{
+    return mMomentum;
+}
+
+inline float Entity::getRadius()
+{
+    return mRadius;
+}
+
+inline bool Entity::isAlive()
+{
+    return mAlive;
+}
+
+//inline void Entity::setGameDead()
+//{
+//    mGameDead = true;
+//}
+//
+//inline void Entity::setRenderDead()
+//{
+//    mRenderDead = true;
+//}
+//
+//inline bool Entity::isGameDead()
+//{
+//    return mGameDead;
+//}
+//
+//inline bool Entity::isRenderDead()
+//{
+//    return mRenderDead;
+//}
+
+/***********************************
+*   Here for readability with a model that
+*   wasn't really built for reference counting.
+*   Just indicates that you are holding a reference
+*   to this entity somewhere
+*************************************/
+inline void Entity::addReference()
+{
+    ++mNumReferences;
+}
+
+/**
+*   Adds to the number of references held to this entity
+*   and returns a reference to this entity.
+**/
+inline Entity* Entity::getReference()
+{
+    ++mNumReferences;
+    return this;
+}
+
+/**
+*   Removes a reference to this entity.
+**/
+inline void Entity::removeReference()
+{
+    --mNumReferences;
+}
+
+/**
+*   Checks to see if there are any references being held to this entity
+**/
+inline bool Entity::hasReferences()
+{
+    return mNumReferences > 0;
+}
+
+#endif
