7a02e9eb8bbd039d2bf5daa3c88377e7b5faa7a5
Muser <ryandmuse@gmail.com>
2011-11-25 13:55:34 -0700

    added widget container stub

---
+++ source/UI/WidgetContainer.h
@@ -0,0 +1,16 @@
+#ifndef WIDGETCONTAINER_H
+#define WIDGETCONTAINER_H
+
+#include <Widget.h>
+
+
+class WidgetContainer : public Widget
+{
+   public:
+      WidgetContainer();
+      virtual ~WidgetContainer();
+   protected:
+   private:
+};
+
+#endif // WIDGETCONTAINER_H
