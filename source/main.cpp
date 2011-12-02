691fc682b6c4e2594be60606d58ee8cf3231b4e9
Kelly Brown <thebuzzsaw@gmail.com>
2011-12-02 15:22:23 -0700

    disabled audio until further notice

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,28 @@
 #include <CGE/Engine.h>
+#include <CGE/Exception.h>
 #include "MainMenuModule.h"
+#include <string>
+using namespace std;
 
 int main(int argc, char** argv)
 {
     CGE::Engine::Settings s;
     s.windowTitle = "paroxysm 0.2";
     s.windowTitle2 = "paroxysm";
+    s.sound = false;
 
     CGE::Engine e(s);
-    e.manage(new MainMenuModule);
+    try
+    {
+        e.manage(new MainMenuModule);
+    }
+    catch (CGE::Exception& ex)
+    {
+        cerr << ex.header << '\n' << ex.message << '\n';
+
+        string s;
+        getline(cin, s);
+    }
 
     return 0;
 }
